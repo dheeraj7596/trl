0f6e9cb7dcde70c3d388d9aeba6be7ad206463a9
dheeraj7596 <dheeraj7596.dm@gmail.com>
2022-11-19 20:01:28 -0800

    0.8  coeff

--- train_notox_cls.py
+++ train_notox_cls.py
@@ -263,7 +263,7 @@ def main():
         "txt_out_min_len": 30,
         "txt_out_max_len": 100,
         "lr": 1.41e-5,
-        "init_kl_coef": 0.2,
+        "init_kl_coef": 0.8,
         "target": 6,
         "horizon": 10000,
         "gamma": 1,
